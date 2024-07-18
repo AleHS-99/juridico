000ca78a6e8eeba16c1117f4c3609eb6b9138131
AleHS-99 <ahsilva745@gmail.com>
2024-07-18 15:13:44 -0400

    def_str

--- nomencladores/models.py
+++ nomencladores/models.py
@@ -7,13 +7,16 @@ class tipo_contrato(models.Model):
         verbose_name_plural = 'Tipos de Contratos'
     id = models.AutoField(primary_key=True)
     t_contrato = models.CharField(max_length=1000,blank=False,null=False, verbose_name="Tipo de Contrato", unique=True)
-
+    def __str__(self):
+        return self.t_contrato
 class forma_pago(models.Model):
     class Meta:
         verbose_name = 'Forma de pago'
         verbose_name_plural = 'Formas de pago'
     id = models.AutoField(primary_key=True)
     f_pago = models.CharField(max_length=1000,blank=False,null=False, verbose_name="Forma de Pago")
+    def __str__(self):
+        return self.f_pago
 
 class departamentos(models.Model):
     class Meta:
@@ -21,3 +24,5 @@ class departamentos(models.Model):
         verbose_name_plural = 'Departamentos'
     id = models.AutoField(primary_key=True)
     departamento = models.CharField(max_length=1000,blank=False,null=False)
+    def __str__(self):
+        return self.departamento
