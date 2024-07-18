41404c70aa8bc45e6ef5fef9bec35223f681af0d
AleHS-99 <ahsilva745@gmail.com>
2024-07-18 10:12:30 -0400

    urls

--- Sjuridico/urls.py
+++ Sjuridico/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("Home.urls")),
     path("Nomencladores/",include("nomencladores.urls")),
+    path("Contratos/",include("contratos.urls")),
 ]
